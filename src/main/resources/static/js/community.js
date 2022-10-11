8b66a510898971624406afd1e3aae67816ecf74b
ZTY <528802333@qq.com>
2022-10-11 22:58:45 +0800

    commit before sleep

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,4 +1,27 @@
 function postComment() {
     const questionId = $('#question_id').val();
-    console.log("questionId");
+    const commentContent = $("#comment_content").val();
+    $.ajax({
+            type: "POST",
+            url: "/comment",
+            contentType: "application/json",
+            data: JSON.stringify({
+                    "parentId": questionId,
+                    "content": commentContent,
+                    "type": 1
+                }
+            ),
+            success: function (response) {
+                if (response.code == 200) {
+                    $("#comment").hide();
+                }else{
+                    alert(response.message);
+                }
+                console.log(response);
+            },
+            dataType: "json"
+        }
+    )
+    console.log(commentContent);
+    console.log(questionId);
 }
